7287c247c7c2d9bd3c1706500f523a6b59b205f9
vicombotile <156225259+vicombotile@users.noreply.github.com>
2024-04-05 06:14:11 +0100

    hhhhhhhhhhhhhhhhhhh=

--- functions.js
+++ functions.js
@@ -3,20 +3,20 @@ function createElem(element, value) {
     txt.innerHTML = value;
     $("body").append(txt)
 }
-function createDetailedElement(element, classNamed, idd, value) {
-    let txt = document.createElement(element)
+function createDetailedElement(element, className, id, value) {
+    let txt = document.createElement(element);
+    $(element + ":empty").attr('id',id);
+    $(element + ":empty").addClass(className);
     txt.innerHTML = value;
-    txt.className = classNamed;
-    txt.id = idd;
 }
 function editElement(elementSelector, value) {
     $(elementSelector).html = value;
 }
 function deleteElement(elementSelector) {
-    $(elementSelector).html = null;
+    $(elementSelector).remove();
 }
 function attributes(elementSelector, attribute, value) {
-    document.querySelector(elementSelector).attribute = value;
+    $(elementSelector).attr(attribute,value)
 }
 function changeCSS(elementSelector, property, value) {
     $(elementSelector).css(property, value);
