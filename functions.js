198af370c80d9bcbf7a16e42ad50c5e901f66b63
vicombotile <156225259+vicombotile@users.noreply.github.com>
2024-04-08 09:42:26 +0100

    DUUUMB

--- functions.js
+++ functions.js
@@ -2,8 +2,8 @@ function createElem(element, value) {
     let txt = $("<"+element+">"+"</"+element+">").html(value);
     $("body").append(txt)
 }
-function editElement(elementSelector, value) {
-    $(elementSelector).html = value;
+function editElement(elementSelector, value) {    
+    $(elementSelector).html(value)
 }
 function createDetailedElement(element, className, id, value) {
     let txt = $("<"+element+" class="+"'"+className+"' "+"id="+"'"+id+"' "+">"+"</"+element+">").html(value);
