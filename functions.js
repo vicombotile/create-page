ff4273452e0aaaa4aec502ae13d6c25a0741fefc
vicombotile <156225259+vicombotile@users.noreply.github.com>
2024-04-08 07:47:15 +0100

    the better way to do it

--- functions.js
+++ functions.js
@@ -1,16 +1,13 @@
 function createElem(element, value) {
-    let txt = document.createElement(element)
-    txt.innerHTML = value;
+    let txt = $("<"+element+">"+"</"+element+">").html(value);
     $("body").append(txt)
 }
 function editElement(elementSelector, value) {
     $(elementSelector).html = value;
 }
 function createDetailedElement(element, className, id, value) {
-    let txt = document.createElement(element);
-    $(element + ":empty").attr('id',id);
-    $(element + ":empty").addClass(className);
-    editElement(element + ":empty", value);
+    let txt = $("<"+element+" class="+"'"+className+"' "+"id="+"'"+id+"' "+">"+"</"+element+">").html(value);
+    $("body").append(txt)
 }
 function deleteElement(elementSelector) {
     $(elementSelector).remove();
